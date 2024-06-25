d8456c08e0a056bdb2c12005a57b41cc35ec8b96
Documenter.jl <documenter@juliadocs.github.io>
2024-06-25 11:58:45 +0000

    build based on e7ccd6b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SchrodingerToolkit","category":"page"},{"location":"#SchrodingerToolkit","page":"Home","title":"SchrodingerToolkit","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SchrodingerToolkit.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SchrodingerToolkit]","category":"page"},{"location":"#SchrodingerToolkit.tfunc-Tuple{Any}","page":"Home","title":"SchrodingerToolkit.tfunc","text":"This is a test function.\n\n\n\n\n\n","category":"method"}]
+}
