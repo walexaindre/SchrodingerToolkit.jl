cd1e36668e92cd8224bedb2f53700245ec733f6d
walexaindre <walexaindre@hotmail.com>
2024-08-02 02:38:41 -0400

    Types upgrade

--- src/Core/ModelPDE/ModelPDE.jl
+++ src/Core/ModelPDE/ModelPDE.jl
@@ -1,8 +1,9 @@
-abstract type SchrodingerPDE{N,RealType} end
 
-"PDE component"
-struct SchrodingerPDEComponent{Tv,Fn,Hn}
-    σ::Tv #Dispersion coefficient
-    f::Fn 
-    ψ::Hn #Initial condition
-end
+@inline has_trapping_potential(Component::SchrodingerPDEComponent{Tv,Fn,InitialCondition,TrappingPotential}) where {Tv,Fn,InitialCondition,TrappingPotential} = if TrappingPotential == Nothing return false else return true end
+
+@inline has_josephson_junction(Component::SchrodingerPDEComponent{Tv,Γv,Fn,InitialCondition,TrappingPotential}) where {Tv,Γv,Fn,InitialCondition,TrappingPotential} = if Γv == Nothing return false else return true end
+
+@inline has_josephson_junction(PDE::SchrodingerPDENonPolynomic{N,Tv,MComp,Potential}) where {N,Tv,MComp,Potential} = any(has_josephson_junction, PDE.components)
+@inline has_josephson_junction(PDE::SchrodingerPDEPolynomic{N,Tv,MComp,Potential,Optimized}) where {N,Tv,MComp,Potential,Optimized} = any(has_josephson_junction, PDE.components)
+@inline has_trapping_potential(PDE::SchrodingerPDENonPolynomic{N,Tv,MComp,Potential}) where {N,Tv,MComp,Potential} = any(has_trapping_potential, PDE.components)
+@inline has_trapping_potential(PDE::SchrodingerPDEPolynomic{N,Tv,MComp,Potential,Optimized}) where {N,Tv,MComp,Potential,Optimized} = any(has_trapping_potential, PDE.components)
---
+++ src/Core/ModelPDE/ModelPDETypes.jl
@@ -0,0 +1,27 @@
+abstract type SchrodingerPDE{N,RealType} end
+
+"PDE component"
+struct SchrodingerPDEComponent{Tv,Γv,Fn,InitialCondition,TrappingPotential} 
+    σ::Tv #Dispersion coefficient => Must be non negative
+    f::Fn #∂∇F/∂xᵢ(x) = f(x)
+    ψ::InitialCondition #Initial condition 
+    V::TrappingPotential # External trapping potential
+    Γ::Γv #Josephson Junction Coefficient => In some documents this coefficient is the same for all components but I guess it can be different under some circumstances.
+end
+
+"Generic handler for non polynomic potentials"
+struct SchrodingerPDENonPolynomic{N,Tv,MComp,Potential} <: SchrodingerPDE{N,Tv}
+    boundaries::NTuple{N,NTuple{2,Tv}}
+    components::MComp
+    F::Potential
+    T::Tv
+end
+
+"Optimized structure for polynomical potentials"
+struct SchrodingerPDEPolynomic{N,Tv,MComp,Potential,Optimized} <: SchrodingerPDE{N,Tv}
+    boundaries::NTuple{N,NTuple{2,Tv}}
+    components::MComp
+    F::Potential
+    N::Optimized
+    T::Tv
+end
