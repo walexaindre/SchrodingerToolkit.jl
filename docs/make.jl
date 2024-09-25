5cca558a46c52c73039429368b3d7cf96c662302
walexaindre <walexaindre@hotmail.com>
2024-09-25 10:10:51 -0400

    Template docs

--- docs/make.jl
+++ docs/make.jl
@@ -20,9 +20,12 @@ makedocs(;
                   "Examples" => ["Notes" => "./Examples/start.md"
                                  "Examples without Josephson junction and trapping potential" => ["2D" => "./Examples/NoTrappingPotentialNoJunction/2D.md"]
                                  "Examples without trapping potential" => ["2D" => "./Examples/NoTrappingPotential/2D.md"]],
-                  "Plotting recipes" => ["Prelude" => "./Plot/howto.md"],
+                  "Plotting recipes" => ["Available recipes" => "./Plot/recipes.md",
+                                         "How to plot" => "./Plot/howto.md"],
                   "Extending the package" => ["Prelude" => "./Extensions/prelude.md",
                                               "Adding a new finite difference scheme" => "./Extensions/fdschemes.md"],
+                  "Benchmark" => ["How to make your own benchmark" => "./Benchmark/howto.md",
+                                  "Benchmark Results" => "./Benchmark/results.md"],
                   "API" => ["Index" => "./API/index.md",
                             "Types" => "./API/API.md"],
                   "Theory" => ["Problem" => "./Theory/Problem.md",
